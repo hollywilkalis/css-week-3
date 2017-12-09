451a846c41f7372d7b807397098fbadf70f43c8f
Holly Wilkalis <holly.wilkalis@gmail.com>
2017-12-08 16:22:02 -0800

    Update button, text, and carousel styles

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,8 @@ $(document).ready(function() {
   $('.carousel').carousel();
   $('.carousel.carousel-slider').carousel({fullWidth: true});
 
+  // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  $('.collapsible').collapsible();
 });
